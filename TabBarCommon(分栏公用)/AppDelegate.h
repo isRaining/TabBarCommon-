4c88a5a142faca96af8265bc95efebbd9fb82cfb
app27 <app27@app27deiMac.local>
2014-07-15 14:33:41 +0800

    Initial Commit

---
+++ TabBarCommon(分栏公用)/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TabBarCommon(分栏公用)
+//
+//  Created by app27 on 14-7-15.
+//  Copyright (c) 2014年 com.sk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TabBarCommon(分栏公用)/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TabBarCommon(分栏公用)
+//
+//  Created by app27 on 14-7-15.
+//  Copyright (c) 2014年 com.sk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
